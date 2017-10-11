718d0d353933ee999dddd23679581f7b1f83963d
Steve Weber <steve.weber@glossybox.com>
2017-10-11 12:53:27 +0200

    added units to header numbers

--- webroot/js/header.js
+++ webroot/js/header.js
@@ -15,18 +15,34 @@ var piggyLabel = svgHeader.append("text")
 var weightTotalLabel = svgHeader.append("text")
     .attr("id","total-weight")
     .attr("class","header-text")
-    .attr("transform", "translate(" + (window.innerWidth - 300) + "," + ($('#header').outerHeight() / 1.5) + ")")
+    .attr("transform", "translate(" + (window.innerWidth - 310) + "," + ($('#header').outerHeight() / 1.5) + ")")
     .attr("text-anchor", "end")
     .style("fill", headerColor)
     .style("font-size", headerFontSize);
 
+svgHeader.append("text")
+    .text('kg')
+    .attr("id","kg")
+    .attr("transform", "translate(" + (window.innerWidth - 280) + "," + ($('#header').outerHeight() / 1.5) + ")")
+    .attr("text-anchor", "end")
+    .style("fill", headerFontColor)
+    .style("font-size", '24px');
+
 var sumTotalLabel = svgHeader.append("text")
     .attr("id","total-sum-pie")
     .attr("class","header-text")
-    .attr("transform", "translate(" + (window.innerWidth - 20) + "," + ($('#header').outerHeight() / 1.5) + ")")
+    .attr("transform", "translate(" + (window.innerWidth - 30) + "," + ($('#header').outerHeight() / 1.5) + ")")
     .attr("text-anchor", "end")
     .style("fill", headerColor)
     .style("font-size", headerFontSize);
+
+svgHeader.append("text")
+    .text('€')
+    .attr("id","currency")
+    .attr("transform", "translate(" + (window.innerWidth - 10) + "," + ($('#header').outerHeight() / 1.5) + ")")
+    .attr("text-anchor", "end")
+    .style("fill", headerFontColor)
+    .style("font-size", '24px');
 
 
 function updateHeader(result) {
